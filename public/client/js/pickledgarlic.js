90e65c61077a666f17c8b8ecbab5e1b42fb93dc4
Gabriel Undan <gundan@hawaii.edu>
2020-04-01 16:40:26 -1000

    Adjusted to accomodate multiple events and VTIMEZONE data

--- public/client/js/pickledgarlic.js
+++ public/client/js/pickledgarlic.js
@@ -1,39 +1,53 @@
 class ICalendarGenerator {
 
-    constructor(title, date, timeStart, timeEnd, location) {
-      this.title = title;
-      this.timeStart = moment(`${date} ${timeStart}`, 'MM-DD-YYYY HHmm').utc().format("YYYYMMDDTHHmmss") + 'Z';
-      this.timeEnd = moment(`${date} ${timeEnd}`, 'MM-DD-YYYY HHmm').utc().format("YYYYMMDDTHHmmss") + 'Z';
-      this.location = location;
-      this.created = moment().utc().format("YYYYMMDDTHHmmss") + 'Z';
-      this.uid = this.generateUUID();
-    }
-  
-    generateUUID() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0,
-          v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-      });
-    }
-  
-    generateCal() {
-      return [
-        `BEGIN:VCALENDAR`,
-        `VERSION:2.0`,
-        `PRODID: Team Picked Garlic ICS 414`,
-        `NAME:${this.title}`,
-        `CALSCALE:GREGORIAN`,
-        `BEGIN:VEVENT`,
-        `DTSTART:${this.timeStart}`,
-        `DTEND:${this.timeEnd}`,
-        `DTSTAMP:${this.created}`,
-        `UID:${this.uid}`,
-        `DESCRIPTION:${this.title}`,
-        `SUMMARY:${this.title}`,
-        `LOCATION:${this.location}`,
-        `END:VEVENT`,
-        `END:VCALENDAR`
-      ].join('\n');
-    }
+	constructor() {
+		this.events = [];
+	}
+
+	// Generates UUID used in the UID field
+	generateUUID() {
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+			var r = Math.random() * 16 | 0,
+				v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		});
+	}
+
+	// Adds event to iCal object
+	addEvent(summary, location, start, end) {
+		this.events.push({
+			"summary": summary,
+			"location": location,
+			"start": moment(start, "MM-DD-YYYY HH:mm").format("YYYYMMDDTHHmmss"),
+			"end": moment(end, "MM-DD-YYYY HH:mm").format("YYYYMMDDTHHmmss")
+		});
+	}
+
+	// Generates contents of iCal file
+	generateCal(tzData) {
+		this.created = moment().utc().format("YYYYMMDDTHHmmss") + "Z";
+		tzData = tzData.split("\n");
+		let tzid = tzData[1].replace("TZID:", "");
+		let ical = [
+			`BEGIN:VCALENDAR`,
+			`VERSION:2.0`,
+			`PRODID: Team Picked Garlic ICS 414`,
+			`CALSCALE:GREGORIAN`
+		].concat(tzData);
+
+		this.events.forEach(event => {
+			ical = ical.concat([
+				`BEGIN:VEVENT`,
+				`DTSTAMP:${this.created}`,
+				`UID:${this.generateUUID()}`
+				`DTSTART;TZID=${tzid}:${event.start}`
+				`DTEND;TZID=${tzid}:${event.end}`
+				`SUMMARY:${event.summary}`
+				`LOCATION:${event.location}`
+				`END:VEVENT`
+			]);
+		});
+
+		return ical.push(`END:VCALENDAR`).join('\n');
+	}
 }
